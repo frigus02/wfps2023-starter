fb8941ed9f178b2608e9a190218d8ad496b91121
Pawel Kozlowski <pkozlowski@google.com>
2023-07-04 17:04:12 +0200

    feat: setup discussion list fetching with mock data

--- solidstart/src/routes/discussions/index.tsx
+++ solidstart/src/routes/discussions/index.tsx
@@ -1,11 +1,29 @@
-import {A, Title} from "solid-start";
+import {createResource, For} from "solid-js";
+import {A, Title, useRouteData} from "solid-start";
+
+export function routeData() {
+  const [discussions] = createResource(async () => {
+    const response = await Promise.resolve([{
+      id: 1,
+      title: 'Title 1',
+      by: 'Anonymous'
+    }]);
+    return response;
+  });
+  return {discussions};
+}
 
 export default function Discussions() {
+  const { discussions } = useRouteData<typeof routeData>();
   return (
     <main>
       <Title>Discussions</Title>
-      <h1>Listing discussions here...</h1>
-      <A href="/discussions/1">Test discussion with id 1</A>
+      <h1>Discussions</h1>
+      <ul>
+      <For each={discussions()}>
+        {(d) => <li><A href={"/discussions/" + d.id}>{d.title}</A> by {d.by}</li>}
+      </For>
+      </ul>
     </main>
   );
 }
