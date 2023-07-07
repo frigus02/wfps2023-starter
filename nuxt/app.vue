c177bbe13002ac7a3b3a8c08491d763906b395b6
Jan Krems <jankrems@google.com>
2023-07-07 14:34:11 -0700

    Add starter app for nuxt

---
+++ nuxt/app.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+const menuItems = computed(() => {
+  const {matched} = useRoute()
+  const matchedName = matched[0]?.name
+
+  return [
+    {
+      name: 'index',
+      href: '/',
+      label: 'Discussions',
+    },
+    {
+      name: 'about',
+      href: '/about',
+      label: 'About',
+    }
+  ].map(item => Object.assign({
+    isActive: matchedName === item.name
+  }, item))
+})
+</script>
+<style scoped>
+nav li.active {
+  font-weight: bold;
+}
+</style>
+<template>
+  <nav>
+    <ul>
+      <li v-for="item in menuItems" :key="item.name" :class="{ active: item.isActive }">
+        <NuxtLink :href="item.href">{{ item.label }}</NuxtLink>
+      </li>
+    </ul>
+  </nav>
+  <main>
+    <NuxtLayout>
+      <NuxtPage/>
+    </NuxtLayout>
+  </main>
+</template>
---
+++ nuxt/server/api/discussions/[number].ts
@@ -0,0 +1,92 @@
+export interface Comment {
+  id: string;
+  author: string;
+  createdAt: string;
+  bodyHTML: string;
+}
+
+export interface Discussion {
+  id: string;
+  number: number;
+  title: string;
+  author: string;
+  createdAt: string;
+  reactionGroups: any;
+  bodyHTML: string;
+  comments: Comment[];
+}
+
+export interface DiscussionResponse {
+  discussion: Discussion;
+}
+
+export default defineEventHandler(async (event): Promise<DiscussionResponse> => {
+  const data: any = await useGraphql(`
+    query discussionDetails($repoOwner: String!, $repoName: String!, $number: Int!) {
+      repository(owner: $repoOwner, name: $repoName) {
+        discussion(number: $number) {
+          id,
+          number
+          title
+          author {
+            login
+          }
+          createdAt
+          reactionGroups {
+            content
+            reactors {
+              totalCount
+            }
+          }
+          bodyHTML
+					comments(first: 10) {
+						edges {
+							node {
+								author {
+									login
+								}
+								createdAt
+								bodyHTML
+							}
+						}
+					}
+        }
+      }
+    }`, {
+      number: Number(event.context.params!['number'])
+    })
+
+  const {
+    repository: {
+      discussion: {
+        id,
+        number,
+        title,
+        author: {login: author},
+        createdAt,
+        reactionGroups,
+        bodyHTML,
+        comments: {edges: comments}
+      }
+    }
+  } = data;
+
+  console.log(reactionGroups);
+
+  return {
+    discussion: {
+      id,
+      number,
+      title,
+      author,
+      createdAt,
+      reactionGroups,
+      bodyHTML,
+      comments: comments.map(({node: {author, createdAt, bodyHTML}}: any) => ({
+        author: author.login,
+        createdAt,
+        bodyHTML
+      }))
+    }
+  }
+})
