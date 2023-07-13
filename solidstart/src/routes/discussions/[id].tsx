cb6215d994ea181a0cfae4c70a16d75f66387724
Pawel Kozlowski <pkozlowski@google.com>
2023-07-13 16:06:54 +0200

    feat: add the discussion details page

--- solidstart/src/routes/discussions/[id].tsx
+++ solidstart/src/routes/discussions/[id].tsx
@@ -1,11 +1,44 @@
-import {Title, useParams} from "solid-start";
+import {For, createMemo} from "solid-js";
+import {RouteDataArgs, Title, useRouteData} from "solid-start";
+import {createServerData$} from "solid-start/server";
+import {REACTION_EMOJI, getDiscussionComments, getDiscussionDetails} from "~/lib/github/discussions";
+
+export function routeData({ params }: RouteDataArgs) {
+  return createServerData$(id => {
+    const discussionId = Number(id);
+    return Promise.all([getDiscussionDetails(discussionId), getDiscussionComments(discussionId)]).then(data => {
+      return {discussion: data[0], comments: data[1]};
+    })
+  }, {key: () => params.id});
+}
 
 export default function Discussions() {
-  const params = useParams();  
+  const discussionAndComments = useRouteData<typeof routeData>();
+  const reactions = createMemo(() => discussionAndComments()?.discussion.reactionGroups.filter((group) => group.totalCount > 0));
+  const discussion = () => discussionAndComments()?.discussion;
+  const comments = () => discussionAndComments()?.comments;
+  
   return (
     <main>
-      <Title>Discussions with id {params.id}</Title>
-      Discussion details (TODO) for id {params.id}
+      <Title> - Discussions</Title>
+      <section>
+        <h1>{discussion()?.title}</h1>
+        <p>by {discussion()?.author} on {discussion()?.createdAt}</p>
+        <div innerHTML={discussion()?.bodyHTML} />
+        <div class="reactions">
+          <For each={reactions()}>
+            {(group) => <button disabled>{REACTION_EMOJI[group.content]}{group.totalCount}</button>}
+          </For>
+        </div>
+        <div class="comments">
+          <h2>Comments</h2>
+          <ul>
+            <For each={comments()}>
+              {(comment) => <div innerHTML={comment.bodyHTML}></div>}
+            </For>  
+		      </ul>
+        </div>
+      </section>
     </main>
   );
 }
